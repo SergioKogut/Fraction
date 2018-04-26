440cacb6b73dcaccf42f3a225468a847c9acd229
Sergio <sergio.kogut@gmail.com>
2018-04-26 22:17:36 +0200

    Start

--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Fraction
 {
+    class Fraction
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public Fraction(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return $"{a}/{b}";
+        }
+
+        
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Fraction a = new Fraction(3, 5);
+
+            Console.WriteLine(a);
+
+
         }
     }
 }
